06f7003cd159d18c43c614b838a1bf53e5c4833b
Rick Gibbed <gibbed@users.noreply.github.com>
2011-02-03 21:17:45 +0000

    Minor organization of projects in solution and a stub for PackSTR.

---
+++ Gibbed.Visceral.PackSTR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gibbed.Visceral.PackSTR
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
