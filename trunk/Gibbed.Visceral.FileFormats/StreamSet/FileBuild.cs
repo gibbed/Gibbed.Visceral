dbfc7fd6ab9b54860e1bbe04210142b086cffd8f
rick <rick@9ff7556a-6e2d-e011-bc74-0030489379b8>
2011-02-03 19:58:21 +0000

    Improving UnpackSTR to make a PackSTR possible.

---
+++ trunk/Gibbed.Visceral.FileFormats/StreamSet/FileBuild.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gibbed.Visceral.FileFormats.StreamSet
+{
+    public enum FileBuild : uint
+    {
+        Default = 0x5393AC01, // "DEFAULT"
+        Debug = 0x143C8453, // "Debug"
+        Temporary = 0x473FCFB4, // "Temp"
+    }
+}
---
+++ trunk/Gibbed.Visceral.FileFormats/StreamSet/FileInfo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gibbed.Helpers;
+using System.IO;
+
+namespace Gibbed.Visceral.FileFormats.StreamSet
+{
+    public class FileInfo
+    {
+        public FileBuild Build;
+        public ushort Unknown04;
+        public ushort Unknown06;
+        
+        public uint Type;
+
+        public uint Unknown0C;
+        public uint Type2;
+        public uint Unknown14;
+        public uint Unknown18;
+
+        public uint TotalSize;
+
+        public string BaseName;
+        public string FileName;
+        public string TypeName;
+
+        public void Deserialize(Stream input, bool littleEndian)
+        {
+            this.Build = (FileBuild)input.ReadValueU32(littleEndian);
+            this.Unknown04 = input.ReadValueU16(littleEndian);
+            this.Unknown06 = input.ReadValueU16(littleEndian);
+
+            this.Type = input.ReadValueU32(littleEndian);
+
+            this.Unknown0C = input.ReadValueU32(littleEndian);
+            this.Type2 = input.ReadValueU32(littleEndian);
+            this.Unknown14 = input.ReadValueU32(littleEndian);
+            this.Unknown18 = input.ReadValueU32(littleEndian);
+
+            this.TotalSize = input.ReadValueU32(littleEndian);
+
+            this.BaseName = input.ReadStringZ();
+            this.FileName = input.ReadStringZ();
+            this.TypeName = input.ReadStringZ();
+        }
+
+        public string GetSaneFileName()
+        {
+            var name = this.FileName;
+            var pos = name.LastIndexOf('\\');
+            
+            if (pos >= 0)
+            {
+                name = name.Substring(pos + 1);
+            }
+
+            if (name.Length > 50)
+            {
+                name = Path.ChangeExtension(name.Substring(0, 50), "." + this.TypeName);
+            }
+            else if (name.Length == 0)
+            {
+                name = Path.ChangeExtension("unknown", "." + this.TypeName);
+            }
+
+            return name;
+        }
+    }
+}
